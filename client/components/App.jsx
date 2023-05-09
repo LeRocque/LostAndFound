606b149a2896bcf337275d71dbefd5bec2e8e54c
LeRocque <t.lerocque@gmail.com>
2023-05-09 16:58:05 -0400

    converted to async await

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,24 +1,36 @@
 import React, { useState } from "react";
 
 const App = () => {
+  // initialize newItem variable and setNewItem func from useState
   const [newItem, setNewItem] = useState("");
 
-  const handleSubmit = (e) => {
+  // declare a function handleSubmit that accepts an event as a param
+  const handleSubmit = async (e) => {
+    // call preventDefault on event to prevent page refresh
     e.preventDefault();
-    fetch("/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ itemName: newItem }),
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.error(error));
-    setNewItem("");
+    // intialize awaited fetch request with a method of POST, headers that send a json, and a body that sends an itemName with the value of newItem as a json
+    try {
+      const result = await fetch("/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ itemName: newItem }),
+      });
+      // initialize data to the awaited result of converting result from a json to an object
+      const data = await result.json();
+      console.log(data);
+      // reset setNewItem input field
+      setNewItem("");
+      // catch errors and console.error them
+    } catch (err) {
+      console.error(err);
+    }
   };
 
+  // initialize handleInputChange func that accepts an event as a param
   const handleInputChange = (e) => {
+    // call setNewItem and pass in the target value of the event as an arg to it
     setNewItem(e.target.value);
   };
 
